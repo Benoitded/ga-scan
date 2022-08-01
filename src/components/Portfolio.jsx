502ea7735f2b2070f153f59a5b7f04552597f59f
Benoit <benoitded82@hotmail.fr>
2022-08-01 20:35:47 +0200

    Add map from json

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,58 +1,35 @@
-import "../styles/Portfolio.css"
-import logo from "../assets/pepe.png"
+import "../styles/Portfolio.css";
+import logo from "../assets/pepe.png";
+import { listAssets } from "../datas/listAssets";
+
 function Portfolio() {
-    const tag="ETH";
-    const price="1706$";
-    const balance="1.5406";
-    return(
-        <section>
-            <div className="portfolio-main-div">
-                <div className="title-portfolio-div">Wallet</div>
-                <div className="portfolio-item header">
-                    <div className="portfolio-item-element assets">Assets</div>
-                    <div className="portfolio-item-element">Price</div>
-                    <div className="portfolio-item-element">Balance</div>
-                    <div className="portfolio-item-element">Value</div>
-                </div>
-                <div className="portfolio-item">
-                    <div className="portfolio-item-element assets">
-                        <img src={logo} alt="Ethereum logo" className="logo-item" />
-                        <div>{tag}</div>
-                    </div>
-                    <div className="portfolio-item-element">{price}</div>
-                    <div className="portfolio-item-element">{balance}</div>
-                    <div className="portfolio-item-element">{price*balance}</div>
-                </div>
-                <div className="portfolio-item">
-                    <div className="portfolio-item-element assets">
-                        <img src={logo} alt="Ethereum logo" className="logo-item" />
-                        <div>{tag}</div>
-                    </div>
-                    <div className="portfolio-item-element">{price}</div>
-                    <div className="portfolio-item-element">{balance}</div>
-                    <div className="portfolio-item-element">{price*balance}</div>
-                </div>
-                <div className="portfolio-item">
-                    <div className="portfolio-item-element assets">
-                        <img src={logo} alt="Ethereum logo" className="logo-item" />
-                        <div>{tag}</div>
-                    </div>
-                    <div className="portfolio-item-element">{price}</div>
-                    <div className="portfolio-item-element">{balance}</div>
-                    <div className="portfolio-item-element">{price*balance}</div>
-                </div>
-                <div className="portfolio-item">
-                    <div className="portfolio-item-element assets">
-                        <img src={logo} alt="Ethereum logo" className="logo-item" />
-                        <div>{tag}</div>
-                    </div>
-                    <div className="portfolio-item-element">{price}</div>
-                    <div className="portfolio-item-element">{balance}</div>
-                    <div className="portfolio-item-element">{price*balance}</div>
-                </div>
+  /*const tag = "ETH";
+  const price = "1706$";
+  const balance = "1.5406";//*/
+  return (
+    <section>
+      <div className="portfolio-main-div">
+        <div className="title-portfolio-div">Wallet</div>
+        <div className="portfolio-item header">
+          <div className="portfolio-item-element assets">Assets</div>
+          <div className="portfolio-item-element">Price</div>
+          <div className="portfolio-item-element">Balance</div>
+          <div className="portfolio-item-element">Value</div>
+        </div>
+        {listAssets.map(({ tag, value, balance, blockchain }) => (
+          <div className="portfolio-item">
+            <div className="portfolio-item-element assets">
+              <img src={logo} alt="Ethereum logo" className="logo-item" />
+              <div>{tag}</div>
             </div>
-        </section>
-    )
+            <div className="portfolio-item-element">{value}</div>
+            <div className="portfolio-item-element">{balance}</div>
+            <div className="portfolio-item-element">{value * balance}</div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
---
+++ src/datas/listAssets.js
@@ -0,0 +1,31 @@
+export const listAssets = [{
+        tag: "ETH",
+        value: '1705.32434',
+        balance: '1.43569',
+        blockchain: "Ethereum"
+    },
+    {
+        tag: "USDC",
+        value: '1',
+        balance: '148.91237',
+        blockchain: "Ethereum"
+    },
+    {
+        tag: "AVAX",
+        value: '27.2419',
+        balance: '540.5',
+        blockchain: "Avalanche"
+    },
+    {
+        tag: "DAI",
+        value: '1',
+        balance: '90.5',
+        blockchain: "BSC"
+    },
+    {
+        tag: "UST",
+        value: '0.21',
+        balance: '540.5',
+        blockchain: "Ethereum"
+    }
+]
